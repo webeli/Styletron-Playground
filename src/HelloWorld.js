3cd981b5fbd3d7e00e209f77bcb5a664399fadb2
Elias Hansen <pe_lias@msn.com>
2017-01-22 15:48:32 +0100

    playing around with styletron and flexbox

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -5,14 +5,15 @@ import {styled} from 'styletron-react';
 // Create a <Title> react component that renders an <h1> which is centered, salmon and sized at 1.5em
 const Title = styled('h1', () => ({
   fontSize: '1.5em',
-  textAlign: 'center',
-  color: 'blue'
+  color: 'blue',
 }));
 
 // Create a <Wrapper> react component that renders a <section> with some padding and a honeydew background
 const Wrapper = styled('section', () => ({
-  padding: '4em',
-  background: 'honeydew'
+  background: 'honeydew',
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'column'
 }));
 
 const Description = styled('p', () => ({
@@ -25,7 +26,7 @@ export default function HelloWorld() {
   // like normal react components – except they're styled!
   return (
     <Wrapper>
-      <Title>Hello World, this is my first styletron!</Title>
+      <Title className="test">Hello World, this is my first styletron!</Title>
       <Description>This is a description to test styletron, and its pretty cool, but i can't edit in style in Chrome DevTools.... Why is that?</Description>
     </Wrapper>
   );
